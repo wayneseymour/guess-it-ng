2b98cb0160bec970878c7d725dbf168baf6b6ef4
Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
2014-10-23 11:15:28 -0400

    realized I could remove the '-module' portion; it seems the injector gets it right

--- guess-it-ng/test/spec/guess-it/guess-it-ctrl.spec.js
+++ guess-it-ng/test/spec/guess-it/guess-it-ctrl.spec.js
@@ -2,7 +2,7 @@ describe('guess it ctrl', function () {
   var scope;
   var guessItCtrl;
 
-  beforeEach(module('guess-it-module'));
+  beforeEach(module('guess-it'));
   beforeEach(inject(function($controller, $rootScope) {
     scope = $rootScope.$new();
     guessItCtrl = $controller('GuessItCtrl', { $scope: scope });
